0e8fed9af84fec295a2f684e12d937a88aa5f589
Aryan Shrivastava <asguy29@gmail.com>
2025-02-24 15:22:24 +0530

    reg form

---
+++ registration-form/src/App.jsx
@@ -0,0 +1,12 @@
+import { RegistrationForm } from "./Registration"
+
+function App() {
+
+  return (
+    <>
+    <RegistrationForm/>
+    </>
+  )
+}
+
+export default App
---
+++ registration-form/src/Registration.jsx
@@ -0,0 +1,11 @@
+export const RegistrationForm=()=>{
+    return(
+        <>
+        <div className="bg-black h-screen w-screen">
+            <div className="flex justify-center items-center h-screen">
+            <div className="mx-auto bg-gradient-to-r from-gray-600 to-slate-700 h-3/5 w-4/5 rounded-2xl"></div>
+            </div>
+        </div>
+        </>
+    )
+}
